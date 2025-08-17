a372d8ddbd5b7799760936fb387b1398e66735cd
afeefakvt <afeefakvt6@gmail.com>
2025-08-17 23:21:12 +0530

    final commit

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,10 @@
+import logo2 from "../assets/Logo2.png";
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
+import twitter from "../assets/twitter.png";
+
+
+
 export default function Footer() {
   return (
     <footer className="bg-[#034833] text-white mt-[80px]">
@@ -6,7 +13,7 @@ export default function Footer() {
         <div>
           <div className="flex items-center gap-2">
             <img
-              src="/src/assets/Logo2.png"
+              src={logo2}
               alt="Visaslots"
               className="w-[155px] h-[44px] md:w-[185px] md:h-[44px]"
             />
@@ -48,9 +55,9 @@ export default function Footer() {
       <div className="bg-[#0E6C4F] px-6 py-4">
         <div className="w-full max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-between">
           <div className="flex gap-4 mb-4 md:mb-0">
-            <a href="#"><img src="/src/assets/facebook.png" alt="Facebook" className="w-6 h-6" /></a>
-            <a href="#"><img src="/src/assets/instagram.png" alt="Instagram" className="w-6 h-6" /></a>
-            <a href="#"><img src="/src/assets/twitter.png" alt="Twitter" className="w-6 h-6" /></a>
+            <a href="#"><img src={facebook} alt="Facebook" className="w-6 h-6" /></a>
+            <a href="#"><img src={instagram} alt="Instagram" className="w-6 h-6" /></a>
+            <a href="#"><img src={twitter} alt="Twitter" className="w-6 h-6" /></a>
           </div>
           <div className="text-sm text-[#FFFFFF] text-center md:text-right">
             © 2025 Visaslots
